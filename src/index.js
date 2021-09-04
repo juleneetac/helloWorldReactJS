f80e8d4fd60dc6744f3a93e60163ef446b3b1a13
julen <gogs@fake.local>
2021-09-04 13:16:35 +0200

    hello world react

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    <h1>Hello Word React live demo!</h1>,
+    <h1>Hello Word React!</h1>,
     document.getElementById("root")
 )
